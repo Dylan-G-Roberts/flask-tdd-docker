525fb10ed8645317a3b6df4b69f29a8993b83b6f
Dylan Roberts <dylangardnerroberts@gmail.com>
2021-11-22 12:21:39 -0500

    created users/{user_id} route + test

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -1,5 +1,6 @@
 import json
-
+from src import db
+from src.api.models import User
 def test_add_user(test_app, test_database):
     client = test_app.test_client()
     resp = client.post(
@@ -59,3 +60,26 @@ def test_add_user_duplicate_email(test_app, test_database):
     data = json.loads(resp.data.decode())
     assert resp.status_code == 400 
     assert 'Sorry. That email already exists' in data['message']
+
+def test_single_user(test_app, test_database):
+    user = User(username='dylan', email='dylan@example.com')
+    db.session.add(user)
+    db.session.commit()
+    client = test_app.test_client()
+    resp = client.get(
+        f'/users/{user.id}'
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert 'dylan' in data['username']
+    assert 'dylan@example.com' in data['email'] 
+
+def test_single_user_incorrect_id(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.get(
+        f'/users/{9999}'
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert 'User 9999 does not exist' in data['message']
+
